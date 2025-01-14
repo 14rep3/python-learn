362f66d1763ce3287ad55532603625f0958d350d
14rep3 <daitakukante@gmail.com>
2025-01-14 23:06:36 +0900

    じゃんけんの修正

--- janken.py
+++ janken.py
@@ -1,43 +1,43 @@
 import random
-i = ["g","y","p"]
+janken_list = ["g","y","p"]
 print("じゃんけんをします")
 print("最初はグーじゃんけん・・・")
-x = input("グーの場合はg、チョキの場合y、パーの場合pを入力してください")
-if x == "g":
-    y = random.choice(i)
-    if y == "g":
-        print("CPUはグーを出しました")
-        print("あいこです")
-    elif y == "y":
-        print("CPUはチョキを出しました")
-        print("プレイヤーの勝ちです")
-    elif y == "p":
-        print("CPUはパーを出しました")
-        print("あなたの負けです")
 
-if x == "y":
-    y = random.choice(i)
-    if y == "g":
-        print("CPUはグーを出しました")
-        print("あいこです")
-    elif y == "y":
-        print("CPUはチョキを出しました")
-        print("プレイヤーの勝ちです")
-    elif y == "p":
-        print("CPUはパーを出しました")
-        print("プレイヤーの負けです")
+# 手の強弱
+strong = {"g":"p","p":"y","y":"g"}
 
-if x == "p":
-    y = random.choice(i)
-    if y == "g":
-        print("CPUはグーを出しました")
-        print("プレイヤーの勝ちです")
-    elif y == "y":
-        print("CPUはチョキを出しました")
-        print("プレイヤーの負けです")
-    elif y == "p":
-        print("CPUはパーを出しました")
-        print("あいこです")
+# 出した手をわかりやすくする
+janken_te = {"g":"グー","p":"パー","y":"チョキ"}
 
-if x != "g" and x != "y" and x != "p":
-    print("半角でg,y,pのどれかを入力してください")
+# プレイヤーの手を決めるための関数
+def player_choice():
+    player = 0
+    n = True
+    while n == True:
+        player = input("グーの場合はg、チョキの場合はy、パーの場合はpを入力してください>>>")
+        if player == "g" or player == "y" or player == "p":
+            n = False
+        else: print("g,y,pのいずれかを半角小文字で入力してください")
+    return player
+
+
+# プレイヤーの手を決定
+player = player_choice()
+
+# CPUの手を決める
+cpu = random.choice(janken_list)
+
+
+# あいこの場合ループから抜け出せないようにする
+while cpu == player:
+        print(f"CPU>>>{janken_te[cpu]}")
+        print("あいこです")
+        print("あいこで・・・")
+        player = player_choice()
+        cpu = random.choice(janken_list)
+
+
+# 勝敗を判定と結果の出力
+if player == strong[cpu]:
+    print(f"CPU>>>{janken_te[cpu]}\nあなたの勝ちです")
+else: print(f"CPU>>>{janken_te[cpu]}\nあなたの負けです")
